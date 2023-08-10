76aa565eee64f873249235c13af822b32d181def
6lackAli3n <abubakarabdullahayo26@gmail.com>
2023-08-10 23:42:22 +0100

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Programming is like building a multilingual puzzle
+ * main - hello world
+ * Description - Programming is like building a multilingual puzzle
  *
  * return: 0 (success)
  */
