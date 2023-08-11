5ce8d7a1619043bdcb24ec2714121fc1374f331f
6lackAli3n <abubakarabdullahayo26@gmail.com>
2023-08-11 01:48:56 +0100

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Entry point
- * Description - Programming is like building a multilingual puzzle
+ * Description - "Programming is like building a multilingual puzzle"
  * return: 0 (success)
  */
 int main(void)
