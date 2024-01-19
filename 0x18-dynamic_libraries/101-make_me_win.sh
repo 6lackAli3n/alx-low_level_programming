a6191d8218f8010a1005e92891f9ddd36761fc9a
6lackAli3n <abubakarabdullahayo26@gmail.com>
2024-01-19 11:50:10 +0100

    wish

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget -p
+wget -p .. https://raw.githubusercontent.com/6lackAli3n/alx-low_level_programming/main/0x18-dynamic_libraries/libhack.so
+export LD_PRELOAD="$PWD/../libhack.so"
